be3ac0d8ed86d5520501a320405657a5191ceb88
Clarity Flowers <clarity@maven.com>
2025-06-05 12:16:40 -0700

    Initial commit

---
+++ index.tsx
@@ -0,0 +1,4 @@
+const Component = () => (
+  <div className="border-mint-500 @4xl:border flex border bg-red-500 lg:bg-red-500" />
+);
+export default Component;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    // // un-comment this to fix sorting the custom color
+    // extend: {
+    //   colors: {
+    //     mint: {
+    //       500: "oklch(0.72 0.11 178)",
+    //     },
+    //   },
+    // },
+  },
+};
